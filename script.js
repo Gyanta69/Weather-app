8462e4bf2043e45492497fdff46a5c862de8035c
Pragyan Khadka <khadkapragyan84@gmail.com>
2024-03-04 08:40:47 +0545

    Modify HTML CSS and JS

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-const apiKey = "Your api key";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=kathmandu";
-async function checkweather(){
-    const response  = await fetch(apiUrl +`&appid=${apiKey}`)
-    const data = await response.json();
-    console.log(data);
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp;
-    document.querySelector(".humidity").innerHTML = data.main.humidity;
-    document.querySelector(".wind").innerHTML = data.wind.speed;
+const apiKey = "adf0fb2a9b61260b4b9cf2437798e2eb";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchbox = document.querySelector(".search input");
+const searchbtn = document.querySelector(".search button");
 
+async function checkweather(city){
+    const response  = await fetch(apiUrl + city +  `&appid=${apiKey}`)
+    if(response.status == 404){
+        document.querySelector(".error").style.display ="block";
+        document.querySelector(".weather").style.display ="none";
+        
+
+      
+    }else{
+    let data = await response.json();
+    let icon = data.weather[0].icon
+
+
+    document.getElementById("icon").src = "https://openweathermap.org/img/wn/" + icon + "@4x.png"
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =  Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/hr";
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display ="none";
+    document.querySelector(".details").style.display ="flex";
+    document.querySelector(".weather-image").style.display ="flex";
+
+    }
 }
-checkweather();
+searchbtn.addEventListener("click",()=>{
+    checkweather(searchbox.value);
+})
